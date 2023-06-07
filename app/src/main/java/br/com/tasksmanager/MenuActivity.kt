31cc59a7664830bf2f214cd90e78c9278e5b911b
gustavonakasone <gustavo.nakasone@hotmail.com>
2023-06-06 21:17:50 -0300

    Versao atual - Projeto

--- app/src/main/java/br/com/tasksmanager/MenuActivity.kt
+++ app/src/main/java/br/com/tasksmanager/MenuActivity.kt
@@ -9,29 +9,48 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.tasksmanager.AddOrderActivity
 import br.com.tasksmanager.databinding.ActivityMenuBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import model.Order
 
 class MenuActivity : AppCompatActivity(), OrderAdapter.OnOrderClickListener {
     private lateinit var binding: ActivityMenuBinding
     private lateinit var adapter: OrderAdapter
     private var selected_item: Order? = null
+    private val auth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        println("Usuario logado com sucesso!")
         val orders = listOf(
             Order("heo3k3ms", "Order 1", "Description", "Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
+            Order("heo3k3ds", "Order 2", "Description","Accepted"),
             Order("heo3k3ds", "Order 2", "Description","Accepted"),
             Order("hfo3k3ms", "Order 3", "Description","Accepted")
         )
 
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userInfo = currentUser?.displayName
 
-
-
+            // Imprimir todas as informações do usuário
+            println("Informacoes do usuario:")
+            println(userInfo)
+        } else {
+            println("Nenhum usuário autenticado")
+        }
 
         adapter = OrderAdapter(this, orders, this)
         binding.ordemServico.layoutManager = LinearLayoutManager(this)
@@ -53,6 +72,11 @@ class MenuActivity : AppCompatActivity(), OrderAdapter.OnOrderClickListener {
                 val intent = Intent(this, OrderDetailActivity::class.java)
                 startActivity(intent)
             }
+        }
+        binding.logOut.setOnClickListener{
+            auth.signOut()
+            finish()
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
     }
@@ -86,6 +110,7 @@ class MenuActivity : AppCompatActivity(), OrderAdapter.OnOrderClickListener {
             Log.e("Error", "Error retrieving orders: $databaseError")
         }
     }
+
 
 
 }
